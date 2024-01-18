5837d9e9ececc144dff5d5cc43c9ebe077f1f513
Hans-Hybani <146996083+Hans-Hybani@users.noreply.github.com>
2024-01-18 12:01:38 +0100

    Fonctionnalité d'envoie de mail ainsi que le message deconfirmation

--- src/Components/button.js
+++ src/Components/button.js
@@ -1,30 +1,45 @@
 import { useState } from 'react';
+import {useRef} from "react";
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-function Buton() {
+function Buton({ onSendEmail }) {
+
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  const handleYesClick = (e) => {
+    // Appeler la fonction onSendEmail du parent
+    if (onSendEmail) {
+      onSendEmail(e);
+    }
+    // Fermer le modal
+    handleClose();
+  };
+
   return (
     <>
       <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
+        Envoyer
       </Button>
 
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Message de confirmation</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Body>Merci d'avoir fait le choix de cette artisan.<br/>
+         En valider voitre choix <strong>Une réponse sera apportée sous 48h.</strong><br/>
+         <span className='green'>Oui, pour valider.</span><br/>
+         <span className='red'>Non, pour faire un autre choix</span>
+        </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
+          <Button variant="danger" onClick={handleClose}>
+            NON
           </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
+          <Button variant="success" onClick={handleYesClick}>
+            OUI
           </Button>
         </Modal.Footer>
       </Modal>
